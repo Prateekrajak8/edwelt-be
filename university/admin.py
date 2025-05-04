5dc5d5162de2873df1b5a0cc11d39d59d328d567
prateeksquapl <prateeksquapl@gmail.com>
2025-05-04 17:00:59 +0530

    admin

--- university/admin.py
+++ university/admin.py
@@ -5,5 +5,5 @@ from university.models import University
 # Register your models here.
 @admin.register(University)
 class UniversityAdmin(admin.ModelAdmin):
-    list_display = ["name", "location", "description", "banner_image_url", "logo_image_url", "brochure_url", "created_at", "updated_at"]
+    list_display = ["name", "location", "description", "created_at", "updated_at"]
     search_fields = ["name"]
