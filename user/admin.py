325467b35fdd997440271ba5f611d9e0d19248f4
prateeksquapl <prateeksquapl@gmail.com>
2025-05-11 12:40:08 +0530

    admin page

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from user.models import UserRegistration
+
 # Register your models here.
+@admin.register(UserRegistration)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "phone_number", "category", "neet_rank", "created_at"]
+    search_fields = ["phone_number"]
