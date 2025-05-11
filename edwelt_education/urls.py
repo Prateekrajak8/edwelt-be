1e2db9edbb8082cbbe541e0e2a4035a8226c7f94
prateeksquapl <prateeksquapl@gmail.com>
2025-05-11 13:11:18 +0530

    news

--- edwelt_education/urls.py
+++ edwelt_education/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('university.urls')),
     path('api/', include('user.urls')),
+    path('news/', include('news.urls')),
 ]
