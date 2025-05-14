524fe6969dde8d32dd52a5ae00f31b512d6c0fd5
prateeksquapl <prateeksquapl@gmail.com>
2025-05-14 11:44:48 +0530

    api

--- university/urls.py
+++ university/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from university.api import UniversityDetailAPIView, UniversityListCreateAPIView
+from university.api import CourseByUniversityView, UniversityDetailAPIView, UniversityListCreateAPIView, UniversityPlacementByUniversityView
 
 urlpatterns = [
     path('universities/', UniversityListCreateAPIView.as_view(), name='university-list-create'),
     path('universities/<int:pk>/', UniversityDetailAPIView.as_view(), name='university-detail'),
+    path('universities/<int:university_id>/placements/', UniversityPlacementByUniversityView.as_view(), name='placements-by-university'),
+    path('universities/<int:university_id>/courses/', CourseByUniversityView.as_view(), name='courses-by-university'),
 ]
