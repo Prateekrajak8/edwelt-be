6275e8853be23a7b6c0255fe19f6248a5c4241fa
prateeksquapl <prateeksquapl@gmail.com>
2025-05-16 15:34:04 +0530

    admin page

---
+++ edwelt_education/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Change admin panel titles
+admin.site.site_header = "Edwelt Education"
+admin.site.site_title = "Edwelt Education Admin Portal"
+admin.site.index_title = "Welcome to Edwelt Education Admin"
